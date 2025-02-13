25a47abf5cd6b6b5ee65d3171a276f1c3b2a6685
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-02-13 12:32:50 -0300

    mudança de toJson.h de diretório

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "algorithms/quicksort.h"  
 #include "algorithms/heapsort.h"   
 #include "algorithms/shellsort.h"  
-#include "algorithms/toJson.h"
+#include "utils/toJson.h"
 #include "utils/loadPythonModule.h"
 #include "utils/calculateTime.h"
 
@@ -19,8 +19,16 @@ int main() {
     loadPythonModule();
     return 0;
     
-    //g++ -I/<pathToPython>/python/<version>/include/python<version> -o main main.cpp -L/<pathToPython>/python/<version>/lib -lpython<version>
-    // you may find this path by $ find ~/ -name Python.h
+    // to compile this file:
+    // $ g++ -I/<pathToPython>/python/<version>/include/python<version> -o main main.cpp -L/<pathToPython>/python/<version>/lib -lpython<version>
+    
+    // you may find this path:
+    // $ find ~/ -name Python.h
+
+    // if you can't find this path, you may need to install python3-dev in your machine, for example:
+    // $ sudo apt install python3-dev (for Ubuntu users)
+    // $ sudo dnf install python3-devel (for Fedora users)
+    
     // you may need to export the path:
     // $ export LD_LIBRARY_PATH=/<pathToPython>/python/<version>/lib:$LD_LIBRARY_PATH
 
