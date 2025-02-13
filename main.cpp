be96e39161d6168d3aed1a92db48141b76f10319
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-02-13 12:21:05 -0300

    adição de instruções de configuração

--- main.cpp
+++ main.cpp
@@ -18,4 +18,10 @@ int main() {
     shellTime(A, exp);
     loadPythonModule();
     return 0;
+    
+    //g++ -I/<pathToPython>/python/<version>/include/python<version> -o main main.cpp -L/<pathToPython>/python/<version>/lib -lpython<version>
+    // you may find this path by $ find ~/ -name Python.h
+    // you may need to export the path:
+    // $ export LD_LIBRARY_PATH=/<pathToPython>/python/<version>/lib:$LD_LIBRARY_PATH
+
 }
